aa3f9f1c535dd6405cc4fd0f7349261fa3cc87d0
d.emelyanov <d.emelyanov@betby.com>
2025-02-05 16:49:40 +0100

    add deploy

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,12 @@ use anyhow::Result;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let db = DB::from_memory().await?;
+    let db_path = std::env::var("DATABASE_URL")
+        .unwrap_or_else(|_| "./data/data.db".to_string());
+    if !std::fs::exists(&db_path).expect("err") {
+        std::fs::File::create(&db_path).expect("DB not created");
+    }
+    let db = DB::new(&format!("sqlite:{}", &db_path)).await?;
     run_bot(db).await?;
     Ok(())
 }
